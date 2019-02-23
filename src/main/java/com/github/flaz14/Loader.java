1593ea2ea5211977a69a9dc5d45de81b76fd81dc
Flaz Fourteen <flazfourteen@gmail.com>
2019-02-23 22:46:17 +0300

    Refactor validations

--- src/main/java/com/github/flaz14/Loader.java
+++ src/main/java/com/github/flaz14/Loader.java
@@ -5,31 +5,32 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 
 import static java.lang.String.format;
 import static java.util.Collections.unmodifiableSet;
-import static java.util.Objects.requireNonNull;
 import static java.util.Set.of;
 
 /**
  *
  */
-public class Loader {
-    public Loader(String urlString) {
-        requireNonNull(urlString, "URL string should not be null.");
-        requireNonEmpty(urlString);
+class Loader {
+    Loader(String urlString) {
+        new UrlRequirement().
+                nonNull(urlString).
+                nonEmpty(urlString);
         try {
             url = new URL(urlString);
         } catch (MalformedURLException onParsing) {
             var message = format("URL string [%s] is malformed.", urlString);
             throw new IllegalArgumentException(message, onParsing);
         }
-        requireSupportedProtocol(url);
+        new UrlRequirement().protocol();
     }
 
-    public BufferedImage load() {
+    BufferedImage load() {
         try {
             BufferedImage image = ImageIO.read(url);
             if (image == null) {
@@ -38,26 +39,10 @@ public class Loader {
                         url);
                 throw new IllegalStateException(message);
             }
-            var width = image.getWidth();
-            if (width > Limitations.IMAGE_WIDTH_IN_PIXELS) {
-                var message = format("The image at URL [%s] " +
-                                "is [%d] pixels wide; " +
-                                "maximum allowed width of an image is equal to [%d] pixels.",
-                        url,
-                        width,
-                        Limitations.IMAGE_WIDTH_IN_PIXELS);
-                throw new IllegalStateException(message);
-            }
-            var height = image.getHeight();
-            if (height > Limitations.IMAGE_HEIGHT_IN_PIXELS) {
-                var message = format("The image at URL [%s] " +
-                                "is [%d] pixels high; " +
-                                "maximum allowed height of an image is equal to [%d] pixels.",
-                        url,
-                        height,
-                        Limitations.IMAGE_WIDTH_IN_PIXELS);
-                throw new IllegalStateException(message);
-            }
+            new ImageRequirement().
+                    width(image).
+                    height(image);
+
         } catch (IOException onRead) {
             var message = format("Failed to read image from URL [%s].", url);
             throw new IllegalStateException(message, onRead);
@@ -65,34 +50,79 @@ public class Loader {
         return null;
     }
 
-    private static void requireNonEmpty(String urlString) {
-        if (urlString.isEmpty()) {
-            throw new IllegalArgumentException("URL string should not be empty.");
+    private class UrlRequirement {
+        UrlRequirement nonNull(String urlString) {
+            Objects.requireNonNull(urlString, "URL string should not be null.");
+            return this;
         }
+
+        UrlRequirement nonEmpty(String urlString) {
+            if (urlString.isEmpty()) {
+                throw new IllegalArgumentException("URL string should not be empty.");
+            }
+            return this;
+        }
+
+        UrlRequirement protocol() {
+            var protocol = url.
+                    getProtocol().
+                    toUpperCase();
+            if (!SUPPORTED_PROTOCOLS.contains(protocol)) {
+                var message = format("Protocol [%s] " +
+                                "specified in URL string [%s] " +
+                                "is not supported; " +
+                                "supported protocols are %s.",
+                        protocol,
+                        url,
+                        SUPPORTED_PROTOCOLS);
+                throw new IllegalArgumentException(message);
+            }
+            return this;
+        }
+
+        // TODO we use set because many other protocols can be supported in future,
+        // TODO also, it simplifies composition of readable and informative exception messages
+        // TODO we wrap it with tree set in order to keep the order of comparison consistent.
+        private final Set<String> SUPPORTED_PROTOCOLS =
+                unmodifiableSet(
+                        new TreeSet<>(of("FILE", "HTTP")));
     }
 
-    private static void requireSupportedProtocol(URL url) {
-        var protocol = url.
-                getProtocol().
-                toUpperCase();
-        if (!SUPPORTED_PROTOCOLS.contains(protocol)) {
-            var message = format("Protocol [%s] " +
-                            "specified in URL string [%s] " +
-                            "is not supported; " +
-                            "supported protocols are %s.",
-                    protocol,
-                    url,
-                    SUPPORTED_PROTOCOLS);
-            throw new IllegalArgumentException(message);
+    private class ImageRequirement {
+        ImageRequirement width(BufferedImage image) {
+            var width = image.getWidth();
+            if (width > Limitations.IMAGE_WIDTH_IN_PIXELS) {
+                var message = String.format("The image at URL [%s] " +
+                                "is [%d] pixels wide; " +
+                                "maximum allowed width of an image is equal to [%d] pixels.",
+                        url,
+                        width,
+                        Limitations.IMAGE_WIDTH_IN_PIXELS);
+                throw new IllegalStateException(message);
+            }
+            return this;
         }
+
+        ImageRequirement height(BufferedImage image) {
+            var height = image.getHeight();
+            if (height > Limitations.IMAGE_HEIGHT_IN_PIXELS) {
+                var message = String.format("The image at URL [%s] " +
+                                "is [%d] pixels high; " +
+                                "maximum allowed height of an image is equal to [%d] pixels.",
+                        url,
+                        height,
+                        Limitations.IMAGE_WIDTH_IN_PIXELS);
+                throw new IllegalStateException(message);
+            }
+            return this;
+        }
+
+        ImageRequirement format(BufferedImage image) {
+            
+            return this;
+        }
+
     }
 
     private final URL url;
-
-    // TODO we use set because many other protocols can be supported in future,
-    // TODO also, it simplifies composition of readable and informative exception messages
-    // TODO we wrap it with tree set in order to keep the order of comparison consistent.
-    private static final Set<String> SUPPORTED_PROTOCOLS =
-            unmodifiableSet(
-                    new TreeSet<>(of("FILE", "HTTP")));
 }
