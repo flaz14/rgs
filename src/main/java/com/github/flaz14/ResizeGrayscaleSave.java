6e8fa55fc420d3a980fabcc12af446e8dd15fcf6
Flaz Fourteen <flazfourteen@gmail.com>
2019-02-23 16:40:36 +0300

    Add a bit of documentation

--- src/main/java/com/github/flaz14/ResizeGrayscaleSave.java
+++ src/main/java/com/github/flaz14/ResizeGrayscaleSave.java
@@ -4,6 +4,7 @@ import picocli.CommandLine;
 
 import static picocli.CommandLine.Command;
 import static picocli.CommandLine.Option;
+import static picocli.CommandLine.Parameters;
 
 /**
  * Написать консольное приложение на Java, которое принимает на вход три параметра:
@@ -26,7 +27,11 @@ import static picocli.CommandLine.Option;
  */
 @Command(
         name = "rgs",
-        description = "Rgs - The stupid image processing tool.",
+        description = "Rgs - the stupid image processing tool." +
+                "\n" +
+                "The tool resizes given image according to desired dimensions and converts it to grayscale. Obviously, the tool" +
+                "doesn't keep aspect ratio. So if you would like to SCALE an image please calculate appropriate dimensions" +
+                "in advance and pass them to the tool explicitly. ",
         mixinStandardHelpOptions = true,
         version = "1.0")
 public class ResizeGrayscaleSave implements Runnable {
@@ -35,10 +40,29 @@ public class ResizeGrayscaleSave implements Runnable {
             description = "Verbose mode. This option is helpful if you would like an interactive session or " +
                     "troubleshoot the application. If you plan to use the application in non-interactive mode (for " +
                     "example, inside a shell-script) you probably don't need to use this option. " +
-                    "Multiple `-v' options increase the verbosity. Default mode is \"off\".")
+                    "Multiple `-v' options increase the verbosity. " +
+                    "\n" +
+                    "This switch is optional. Default mode is \"off\".")
     private boolean[] verbose = new boolean[0];
 
+    @Parameters(
+            arity = "1",
+            paramLabel = "URL",
+            description = "URL to image to be processed. The should include protocol name. " +
+                    "Currently two protocols are supported: local files (e.g. `file://path/to/image') and HTTP (e.g. " +
+                    "`http://mysite.com/image')." +
+                    "\n" +
+                    "This parameter is mandatory.")
+    private String url;
 
+    @Parameters(
+            arity="1",
+            paramLabel = "WIDTH",
+            description = "Width of the output image"
+    )
+    private int width;
+
+    private int height;
 
     public void run() {
     }
